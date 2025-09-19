0466d8016b2adbfa8db3c18f595e2e05bdc59410
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-19 15:17:15 +0530

    variables+arrguments

--- Special_var.sh
+++ Special_var.sh
@@ -9,3 +9,9 @@ echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
 sleep 50 &
 echo "PID of the last command in background is: $!"
+
+#variables
+name="venkat"
+age="30"
+village="hyd"
+occupation="IT Professional"
