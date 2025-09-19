bf45a73b68d312fa3b85b3816db2d705296b93d4
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-19 10:26:00 +0530

    variables

--- Variables.sh
+++ Variables.sh
@@ -6,4 +6,4 @@ village="hyd"
 occupation="IT Professional"
 
 echo "My Details
-Name:$name ,Age=$age, Village=$village, Occupation=$occupation "
+Name: $name ,Age= $age, Village= $village, Occupation= $occupation "
