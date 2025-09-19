380ab1537b2c7bc5b403b3656075ca19006e5767
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-19 12:52:19 +0530

    variables+arrguments

--- variables-Arrguments.sh
+++ variables-Arrguments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-name=$0
-age=$1
+name=$1
+age=$2
 village=$3
 occupation=$4
 
 echo "My Details
-Name: $0 ,Age= $2, Village= $3, Occupation= $4 "
+Name: $1 ,Age= $2, Village= $3, Occupation= $4 "
