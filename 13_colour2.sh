45036c9e94a90a3ff946cf7c6670d30ed915d85a
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-23 16:30:47 +0530

    Colour2

---
+++ 13_colour2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Please run this script with root privelage"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Installation of $2 is Failure"
+    else
+        echo "Installation of $2 is Success"
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
