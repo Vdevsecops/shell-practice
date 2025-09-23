8702b6cf6a67fc6697499bb107d60e11c804b3d8
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-23 11:05:13 +0530

    app_install

---
+++ 10_function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Please run the script with root priveleage"
+    exit 1
+fi
+
+VALIDATE(){ # function receives the inputs from args like shell script args
+    if [ $1 -ne 0 ]; then
+        echo  "Error :: installation of $2 is failure"
+        exit 1
+    else
+        echo "Installation $2 is Success"
+
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
