78850a8d54cdcc651b4abd935e55b377fa57a391
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-23 17:19:17 +0530

    Colour2

--- 13_colour2.sh
+++ 13_colour2.sh
@@ -20,5 +20,20 @@ if [ $? -ne 0 ];then
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else
-    echo "MySQL Package already Installed"
+    echo "MySQL Package Is Already Installed"
 fi
+
+dnf list installed nginx
+if [ $? -ne 0]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "Nginx Package is Already Installed"
+
+
+dnf list installed python3
+    if [ $? -ne 0];then
+        dnf install python3 -y
+        VALIDATE $? "Python3"
+    else 
+        echo "Python3 Package Is Already Installed"
