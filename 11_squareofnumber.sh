555a4d7dc8cd9a3841305c0f87e2c6c07d643c2f
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-23 12:36:05 +0530

    update

--- 11_squareofnumber.sh
+++ 11_squareofnumber.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# square(){
-#     sqr=$Number**2
-#     echo "square of your number is $sqr"
-# }
-
-# Number=$(read -p "Please enter the number: ")
-
-# sqare $Number
-
-
-# Function to calculate square
-square() {
-  echo $(($1 * $1))
+square(){
+    sqr=$Number**2
 }
 
-# Ask user for a number
-echo "Enter a number:"
-read num
+Number=$(read -p "Please enter the number: ")
 
-# Call function and show result
-echo "Square is: $(square $num)"
+sqare $Number
+echo "square of your number is $sqr"
+
+# Function to calculate square
+# square() {
+#   echo $(($1 * $1))
+# }
+
+# # Ask user for a number
+# echo "Enter a number:"
+# read num
+
+# # Call function and show result
+# echo "Square is: $(square $num)"
