56fa12374cb6801f04d9b8d4e808c8b379096526
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-23 11:56:59 +0530

    update

--- 11_squareofnumber.sh
+++ 11_squareofnumber.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Please enter your numner"
-Number=read 
-
 square(){
     sqr=$Number**2
     echo "square of your number is $sqr"
 }
+
+read -p "Please enter the number: $Number"
