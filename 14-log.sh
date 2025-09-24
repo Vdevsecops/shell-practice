e8ab8fc556dad5eefc06ae5eaf0c63d6450813eb
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-24 10:37:10 +0530

    Colour2

--- 14-log.sh
+++ 14-log.sh
@@ -1,53 +1,30 @@
 #!/bin/bash
 
-userid=$(id -u)
+USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-Log_Folder="/var/log/Shell-Scripting"
-Script_name=$(echo $0 | cut -d "." -f1)
-Log_File="$Log_Folder/$Script_name.log"
-mkdir -p $Log_Folder
-echo "Script Started exicuting on $(date)"
-
-
-if [ $userid -ne 0 ]; then
-    echo "Please run this script with root privelage"
+if [ $USERID -ne 0 ]; then
+    echo "Please run Script With Root Privelage"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "Installation of $2 is $R Failure $N"
+    if [$1 -ne 0];then
+        echo -e "installation of $2 is $R Failure $N"
         exit 1
     else
-        echo -e "Installation of $2 is $G Success $N"
+        echo -e "Installation of $2 is $G SUCCESS $N"
     fi
 }
 
-dnf list installed mysql &>>$Log_File
-    if [ $? -ne 0 ];then
-        dnf install mysql -y &>>$Log_File
-        VALIDATE $? "MySQL"
-    else
-        echo -e "MySQL Package Is Already Installed $Y Skipping the Step $N"
-    fi
-
-dnf list installed nginx &>>$Log_File
-    if [ $? -ne 0 ]; then
-        dnf install nginx -y &>>$Log_File
-        VALIDATE $? "nginx"
-    else
-        echo -e "Nginx Package is Already Installed $Y Skipping the Step $N"
-    fi
-
-dnf list installed python3 &>>$Log_File
-    if [ $? -ne 0 ];then
-        dnf install python3 -y &>>$Log_File
-        VALIDATE $? "Python3"
-    else 
-        echo -e "Python3 Package Is Already Installed $Y Skipping the Step $N"
-    fi
-
+dnf list installed mysql
+if [$? -ne 0];then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else 
+    echo "MySQl Package is already Installed $Y Skipping $N"    
+fi
