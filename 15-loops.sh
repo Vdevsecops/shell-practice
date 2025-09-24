5f1ecbc740dbc42faa5f35b7fd5f78eb22987363
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-24 16:40:14 +0530

    Colour2

---
+++ 15-loops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell_log"
+LOG_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$LOG_NAME.log"
+mkdir -p $LOG_FOLDER
+
+echo "Script Started Executed on $(date)" &>>$LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "Please run Script With Root Privelage"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo -e "installation of $2 is $R Failure $N"
+        exit 1
+    else
+        echo -e "Installation of $2 is $G SUCCESS $N"
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package is Already Installed ...$Y Skipping $N"
+    fi
+done
