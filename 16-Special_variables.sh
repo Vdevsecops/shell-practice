aa2909d3a65ebecb81defd70905259d2d3f11e9b
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-26 10:50:49 +0530

    Special Variables

--- 16-Special_variables.sh
+++ 16-Special_variables.sh
@@ -5,3 +5,8 @@ echo "You passed $# arguments."
 
 # '$0' – Script name
 echo "This script's name is: $0"
+
+# '$1 to $9' – Positional parameters
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Third argument: $3"
