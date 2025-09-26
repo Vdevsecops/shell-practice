8be24bb26bc6b96c27b12f6e0c4595f11be57a7c
CH VENKATRAVI <VENKATRAVI.CHL@GMAIL.COM>
2025-09-26 11:54:33 +0530

    Special Variables

--- 16-Special_variables.sh
+++ 16-Special_variables.sh
@@ -43,7 +43,7 @@ echo "PID of last background process: $!"
 echo "Last argument of previous command: $_"
 # For better effect, try running:
 ls /etc
-sh "$0"
+sh 
 echo "$(pwd)"
 echo "$(pwd)"
 echo "$(pwd)"
